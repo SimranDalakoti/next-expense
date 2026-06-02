cb7f4507c4926a51eba1a6b6ec4ae173006bf58c
simran <simrandalakoti15@gmail.com>
2026-06-02 09:51:37 +0530

    clerk

---
+++ lib/checkUser.ts
@@ -0,0 +1,26 @@
+import { currentUser } from "@clerk/nextjs/server";
+import prisma from "./prisma";
+
+export const checkUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+  const loggedInUser = await prisma.user.findUnique({
+    where: {
+      clerkUserId: user.id,
+    },
+  });
+  if (loggedInUser) {
+    return loggedInUser;
+  }
+  const newUser = await prisma.user.create({
+    data: {
+      clerkUserId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0]?.emailAddress,
+    },
+  });
+  return newUser;
+};
